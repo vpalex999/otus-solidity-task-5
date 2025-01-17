833000f92cdc1da66decef94a2cfa3cd68840f8c
vpalex999 <vpalex999>
2025-01-17 21:41:50 +0500

    init

---
+++ scripts/Bank.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.28;
+
+import './INativeBank.sol';
+
+contract Bank is INativeBank {
+    mapping (address => uint) private  balances;
+
+    function balanceOf(address account) external view returns(uint256){
+        return balances[account];
+    }
+
+    function deposit() external payable {
+        balances[msg.sender] += msg.value;
+    }
+
+    function withdraw(uint256 amount) external payable  {
+        balances[msg.sender] = balances[msg.sender] - amount;
+        address payable _to = msg.sender
+
+    }
+}
